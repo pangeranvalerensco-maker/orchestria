f34ed27e7be72d53ec703d87f968dc33673a993b
pangeran valerensco <pangeranvalerensco@gmail.com>
2026-05-26 14:49:03 +0700

    chore: initialize auth-service

---
+++ backend/auth-service/src/main/java/com/pangeranvalerensco/orchestria/auth_service/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pangeranvalerensco.orchestria.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
