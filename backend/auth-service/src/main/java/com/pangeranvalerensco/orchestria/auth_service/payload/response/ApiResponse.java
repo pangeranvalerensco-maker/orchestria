bbf31d6267297ac9734ca68420773d1348930c6c
pangeran valerensco <pangeranvalerensco@gmail.com>
2026-05-29 22:39:02 +0700

    chore: organize auth-service structure and add api response wrapper

---
+++ backend/auth-service/src/main/java/com/pangeranvalerensco/orchestria/auth_service/payload/response/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.pangeranvalerensco.orchestria.auth_service.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    
+}
